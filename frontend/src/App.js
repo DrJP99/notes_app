80ad8da006d8b6f2fabea75e45eb83d35abd7f75
DrJP99 <jpgzz1999@gmail.com>
2024-05-20 13:14:55 -0600

    frontend initial page

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import NoteCard from "./NoteCard"
+
+const App = () => {
+	return (
+		<div className="App">
+			<h1>My Notes App</h1>
+			<NoteCard />
+			<NoteCard />
+			<NoteCard />
+		</div>
+	)
+}
+
+export default App
---
+++ frontend/src/NoteCard.js
@@ -0,0 +1,14 @@
+const NoteCard = () => {
+	return (
+		<div className="note-card">
+			<div className="card-header">
+				<h3 className="card-title">Title</h3>
+			</div>
+			<div className="card-body">
+				<p className="card-body">This is a card body</p>
+			</div>
+		</div>
+	)
+}
+
+export default NoteCard
