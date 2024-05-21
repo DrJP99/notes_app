7096bc7a2a45894467eba07c139e1014cf79af04
DrJP99 <jpgzz1999@gmail.com>
2024-05-21 14:45:59 -0600

    added button to see archived notes in user page

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -5,18 +5,34 @@ import NoteCard from './NoteCard'
 
 const User = () => {
 	const user = useSelector((state) => state.user)
-	const [notes, setNotes] = useState([])
+	const [hide, setHide] = useState(true) // hides archived notes
+	const [allNotes, setAllNotes] = useState([])
+	const [notes, setNotes] = useState([]) // filtered notes
 
 	useEffect(() => {
 		noteService
 			.getUserNotes(user)
-			.then((res) => setNotes(res))
+			.then((res) => setAllNotes(res))
 			.catch((err) => console.error(err.message))
 	}, [user])
+
+	useEffect(() => {
+		if (hide) {
+			setNotes(allNotes.filter((note) => note.archived === false))
+		} else {
+			setNotes(allNotes)
+		}
+	}, [hide, allNotes])
 
 	return (
 		<div>
 			<h1>{user}</h1>
+			<button
+				className={`btn btn-${hide ? 'good' : 'warning'}`}
+				onClick={(_) => setHide(!hide)}
+			>
+				{hide ? 'See' : 'Hide'} Archived Notes
+			</button>
 			{notes ? (
 				notes.map((note) => <NoteCard key={note.note_id} note={note} />)
 			) : (
