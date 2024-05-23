129a046f9e5c99187216afa631deb760773687db
DrJP99 <jpgzz1999@gmail.com>
2024-05-22 19:33:19 -0600

    readme: Finished README file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import NoteForm from './components/NoteForm'
 import Navbar from './components/Navbar'
